e320af7e8c56611104440e53e014685df746ee3e
satya <satya.nani.40@gmail.com>
2015-05-17 19:25:18 +0530

    comment

--- static/app/scripts/controllers/enter_interests.js
+++ static/app/scripts/controllers/enter_interests.js
@@ -51,6 +51,7 @@ angular.module('weberApp')
                     }
 
                     $scope.currentUser.questions.push({'questionid':question, 'answer': ans});
+                    console.log('pushed answereds', $scope.currentUser.questions)
                     $scope.questions.updateAnswer(question, ans, $scope.currentUser._id);
                     return;
                 }
